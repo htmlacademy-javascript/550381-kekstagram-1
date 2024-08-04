76e6fc407ec6236280c43701b8a6212100c75295
Keks <keks@htmlacademy.ru>
2024-08-04 20:48:47 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/create-data.js
@@ -0,0 +1,53 @@
+import {getRandomInteger, getRandomArrayElement} from './utils.js';
+
+const IMAGECOUNT = 25;
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = [
+  'Иван',
+  'Андрей',
+  'Ангелина',
+  'Марк',
+  'Захарий',
+  'Веньямин',
+  'Слава',
+  'Святослас',
+  'Станислав',
+  'Димитрий',
+  'Тодор'
+];
+const DESCRIPTIONS = [
+  'Моя бабушка случайно чихнула',
+  'Получилась фотография',
+  'Я поскользнулся на банановой кожуре',
+  'Уронил фотоаппарат на кота',
+  'Поймал момент',
+  'А вот и солнце',
+  'Мои родные',
+  'А вот и я',
+];
+
+const createCommentObject = (commentIndex) => ({
+  id: 100 + commentIndex,
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: Array.from({length: getRandomInteger(1, 2)}, () => getRandomArrayElement(MESSAGES)).join(' '),
+  name: getRandomArrayElement(NAMES)
+});
+
+
+const createImageObject = (dataIndex) => ({
+  id: dataIndex + 1,
+  url: `photos/${dataIndex + 1}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({length: getRandomInteger(3, 6)}, (_, index) => createCommentObject(index))
+});
+
+const createData = () => Array.from({length: IMAGECOUNT}, (_, index) => createImageObject(index));
+
+export {createData};
---
+++ 5/js/main.js
@@ -0,0 +1,5 @@
+import { createData } from './create-data.js';
+import { renderThumbnails } from './thumbnails.js';
+const pictures = createData();
+
+renderThumbnails(pictures);
