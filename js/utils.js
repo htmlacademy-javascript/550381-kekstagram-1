c91de93544a1f5d376fc907ff0c5d61897b32880
IlyaMuralha <muralhakuzmenko@gmail.com>
2024-10-14 04:05:57 +0500

    Задание Надо подкачаться выполнено

--- js/utils.js
+++ js/utils.js
@@ -1,25 +1,45 @@
-const HTMLBODY = document.querySelector('body');
-
-// функция для получения целого положительного рандомного числа в заданном диапозоне
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
+const HTML_BODY = document.querySelector('body');
+const ALERT_SHOW_DELAY = 5000;
+const ScaleParams = {
+  MIN_SCALE: 25,
+  MAX_SCALE: 100,
+  STEP_SCALE: 25
 };
 
-// функция для возвращения случайного элемента из массива
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
-
 const isEnterKey = (evt) => evt.key === 'Enter';
 
+const createAlert = (container, message) => {
+  container.style.zIndex = '100';
+  container.style.position = 'absolute';
+  container.style.left = 0;
+  container.style.top = 0;
+  container.style.right = 0;
+  container.style.backgroundColor = 'rgb(60, 54, 20)';
+  container.style.color = 'rgb(255, 231, 83)';
+  container.style.lineHeight = '40px';
+  container.style.padding = '30px 10px';
+  container.style.fontSize = '32px';
+  container.style.fontWeight = '700';
+  container.style.textAlign = 'center';
+  container.textContent = message;
+
+  HTML_BODY.append(container);
+};
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  createAlert(alertContainer, message);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_DELAY);
+};
+
 export {
-  HTMLBODY,
-  getRandomInteger,
-  getRandomArrayElement,
+  HTML_BODY,
   isEscapeKey,
-  isEnterKey
+  isEnterKey,
+  showAlert,
+  ScaleParams
 };
